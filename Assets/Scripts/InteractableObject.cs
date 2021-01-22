6b8dc02f310ddf48a4d70486d49b86e4d4ee86e7
mattboy2001 <57643755+mattboy2001@users.noreply.github.com>
2021-01-22 18:04:06 +0000

    Object interaction with rendering outline

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace cakeslice {
+    public class InteractableObject : MonoBehaviour {
+
+
+        #region Private Fields
+
+        Outline outline;
+
+        #endregion
+
+
+
+        void Start()
+        {
+            outline = GetComponent<Outline>();
+            outline.enabled = false;
+        }
+
+
+        void Update()
+        {
+            if (outline.enabled)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+
+                    //TODO Inventory management
+                    Destroy( gameObject );
+                }
+            }
+        }
+
+
+        private void OnTriggerEnter() {
+            outline.enabled = true;
+            Debug.Log("Press F");
+        }
+
+        private void OnTriggerExit() {
+            outline.enabled = false;
+        }
+    }
+}
+
+
